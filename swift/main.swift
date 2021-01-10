23ec4975d82dde53c1d7cfb4ae56ebe674ee9f80
EU-Create <github@eu-create.net>
2021-01-10 17:31:35 +0900

    Add Swift file

---
+++ swift/main.swift
@@ -0,0 +1,45 @@
+func addition(a: Double, b: Double) {
+    print("a + b =", a + b)
+}
+
+func subtraction(a: Double, b: Double) {
+    print("a - b =", a - b)
+}
+
+func multiplication(a: Double, b: Double) {
+    print("a x b =", a * b)
+}
+
+func division(a: Double, b: Double) {
+    print("a / b =", a / b)
+}
+
+print("Four arithmetic operations on a and b\nEnter values\na = ", terminator: "")
+var a = Double(readLine()!)!
+print("b = ", terminator: "")
+var b = Double(readLine()!)!
+print("Which calculations do you want to output?")
+print("0. addition")
+print("1. subtraction")
+print("2. multiplication")
+print("3. division")
+print("Except 0 to 3. All")
+print("Enter a number: ", terminator: "")
+var outputFormat = Int(readLine()!)!
+print("a =", a, ", b =", b)
+
+switch outputFormat {
+    case 0:
+        addition(a: a, b: b)
+    case 1:
+        subtraction(a: a, b: b)
+    case 2:
+        multiplication(a: a, b: b)
+    case 3:
+        division(a: a, b: b)
+    default:
+        addition(a: a, b: b)
+        subtraction(a: a, b: b)
+        multiplication(a: a, b: b)
+        division(a: a, b: b)
+}
